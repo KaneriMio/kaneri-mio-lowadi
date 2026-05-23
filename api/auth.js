9aea802b19f9627714460cce0c13a84ef040d2a9
KaneriMio <mikyki.01@gmail.com>
2026-05-23 20:15:09 +0500

    Delete api directory

--- api/auth.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  const { client_id } = req.query;
-  res.redirect(`https://github.com{client_id}&scope=repo,user`);
-}
--- api/callback.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { code } = req.query;
-  res.send(`
-    <script>
-      const res = { token: "${code}", provider: "github" };
-      window.opener.postMessage(JSON.stringify(res), window.location.origin);
-    </script>
-  `);
-}
